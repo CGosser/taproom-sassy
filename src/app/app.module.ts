5138e9e28f073b2152d7aec76b1ed23df829731d
Chris Gosser and Evan Filkins <chris@>
2018-10-18 16:12:19 -0700

    Add two-way binding to Edit Form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { KegListComponent } from './keg-list/keg-list.component';
 import { EditKegComponent } from './edit-keg/edit-keg.component';
@@ -12,7 +12,8 @@ import { EditKegComponent } from './edit-keg/edit-keg.component';
     EditKegComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
