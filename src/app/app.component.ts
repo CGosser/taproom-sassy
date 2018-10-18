7a9f26cbad31feeb20bf156f1cb3261caec75df5
Chris Gosser and Evan Filkins <chris@>
2018-10-18 11:35:36 -0700

    create project files and object constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sassy';
+  title = 'tAppRoot';
+
+  kegs: Keg[] = [
+    new Keg('Beak Breaker IPA', 'Pelican Brewing', 5.00, 9.6),
+    new Keg('Samuel Adams American Lager', 'Sam Adams Brewing', 3.50, 5.0),
+    new Keg('Terminator Stout', 'McMenamins', 6, 6.45),
+    new Keg('Thundercone', 'McMenamins', 7, 6.19)
+  ]
 }
---
+++ src/app/models/keg.model.ts
@@ -0,0 +1,4 @@
+export class Keg {
+  public volume: number = 128;
+  constructor(public name: string, public brand: string, public price: number, public abv: number) {}
+}
